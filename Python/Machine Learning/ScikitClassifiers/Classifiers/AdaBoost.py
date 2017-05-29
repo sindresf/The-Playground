a451d5db0d3baaa98a213d7cb08bd24e3f82c68e
sindresf <sindresfjermestad@gmail.com>
2017-05-29 13:31:04 +0200

    ADAd boost

---
+++ Python/Machine Learning/ScikitClassifiers/Classifiers/AdaBoost.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sklearn import model_selection
+from sklearn.ensemble import AdaBoostClassifier
+
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/pima-indians-diabetes/pima-indians-diabetes.data"
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+
+df = pd.read_csv(url,names=names)
+array = df.values
+X = array[:, 0:8]
+y = array[:, 8]
+
+seed = 21
+num_trees = 30
+kfold = model_selection.KFold(n_splits=10, random_state=seed)
+model = AdaBoostClassifier(n_estimators=num_trees, random_state=seed)
+results = model_selection.cross_val_score(model, X, y, cv=kfold)
+
+print('results: ')
+print(results)
+print()
+print('mean: ' + str(results.mean()))
