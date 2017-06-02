dd4b85c806e33a6c43ebab0aa573bbf0e36c29c9
sindresf <sindresfjermestad@gmail.com>
2017-06-02 17:11:57 +0200

    made a project for silly intra-company scoring for the summer feast

---
+++ Python/Machine Learning/SummerFeast/SummerChallengeScorer/SummerChallengeScorer/SummerChallengeScorer.py
@@ -0,0 +1,22 @@
+import numpy as np
+from keras.layers import LSTM
+import bokeh as plt
+
+#first time setup
+#make ranges with a larger time step
+#make stepwise sampling of expo-func
+#fuck with it manually to create slight bump around "some timing"
+#save in csv file for later runs
+
+#Second time setup, or: how to train your LSTM
+#train lstm on ranges and expo-func
+#save lstm for direct questioning on other computers
+#make automatic primer given alcohol level
+
+#Third time setup
+#have network trained
+#prime it
+#live plot second for second the output with bokeh
+#have a pause or something to Mark the time actually taken through user input
+# update is via while loop i think, so can interrupt there, or check for "marker input"
+#keep the finalized plots for later revue by participants.
