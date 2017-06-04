eff99692339c4425a94f26fc6ae64f1ac475e1af
sindresf <sindresfjermestad@gmail.com>
2017-06-04 15:48:30 +0200

    data maker saves to csv. csv is loaded and plotted with bokeh

--- Python/Machine Learning/SummerFeast/SummerChallengeScorer/SummerChallengeScorer/create_data.py
+++ Python/Machine Learning/SummerFeast/SummerChallengeScorer/SummerChallengeScorer/create_data.py
@@ -3,20 +3,10 @@ from bokeh.plotting import figure, output_file, show
 
 #DEFINE INNER FUNCTIONS
 def inv_log_func(x, a, b):
-    return (a / (np.log(b * x)))
+    return ((a*starting_score) / (np.log(b * x)))
 
 def bump_func(x,e):
     return (e * np.sin(x - np.pi / 2)) + e
-
-def plot_sin(steps):
-    sin = sin_vals(steps)
-
-    output_file("sine_line.html")
-    sp = figure(width=800, height=800)
-    sin_step = (np.pi * 2.0) / steps
-    x_range = np.arange(0,np.pi * 2.0 + 0.1,sin_step)
-    sp.line(x_range,sin,legend='sin_bump', alpha=0.7, line_width=4, color='green')
-    show(sp)
 
 def sin_vals(ampl,steps):
     sin_step = (np.pi * 2.0) / steps
@@ -25,9 +15,9 @@ def sin_vals(ampl,steps):
     return sin_vals
 
 def make_line_trio_data(ami,a,ama,bmi,b,bma):
-    min_data = [inv_log_func(x,ami,bmi) for x in plot_range]
-    mid_data = [inv_log_func(x,a,b) for x in plot_range]
-    max_data = [inv_log_func(x,ama,bma) for x in plot_range]
+    min_data = [int(inv_log_func(x,ami,bmi)) for x in data_range]
+    mid_data = [int(inv_log_func(x,a,b)) for x in data_range]
+    max_data = [int(inv_log_func(x,ama,bma)) for x in data_range]
     return min_data,mid_data,max_data
 
 def add_bump_func(vals,ampl,frm,to):
@@ -36,81 +26,79 @@ def add_bump_func(vals,ampl,frm,to):
     for i in range(frm,to):
         sini += 1
         vals[i] += bump_vals[sini] * 0.5
+        vals[i] = int(vals[i])
 
 def add_bump_trio(ampl,xmi,x,xma,frm,to):
     add_bump_func(xmi,ampl,frm,to)
     add_bump_func(x,ampl,frm,to)
     add_bump_func(xma,ampl,frm,to)
 
-def add_trio_to_plot(plot, ymi,y,yma):
-    pb.line(plot_range,ymi, alpha=0.6 + np.random.normal(0,0.2), line_width=4, color='green')
-    pb.line(plot_range,y, alpha = 0.6 + np.random.normal(0,0.2), line_width = 4, color = 'navy')
-    pb.line(plot_range,yma, alpha=0.6 + np.random.normal(0,0.2), line_width=4, color='red')
-
-def make_trio(a,b,c,d,ampl,plot):
+def make_trio(a,b,c,d,ampl):
     mini,mid,maxi = make_line_trio_data(a[0],a[1],a[2],b[0],b[1],b[2])
     bump_start = int(len(mid) * c)
     bump_end = len(mid) - d
     add_bump_trio(ampl,mini,mid,maxi,bump_start,bump_end)
-    add_trio_to_plot(plot,mini,mid,maxi)
+    return [a[0]] + mini,[a[1]] + mid,[a[2]] + maxi
 
-def make_trios(count,plot):
+def get_az():
+    ami = np.random.uniform(low=0.6, high=0.9)
+    amd = np.random.uniform(low=0.9, high=1.1)
+    ama = np.random.uniform(low=1.1, high=1.4)
+    return [ami,amd,ama]
+
+def get_bz():
+    bmi = b * np.random.uniform(low=0.8, high=0.95)
+    bmd = b * np.random.uniform(low=0.95, high=1.05)
+    bma = b * np.random.uniform(low=1.75, high=1.225)
+    return [bmi,bmd,bma]
+    
+
+def make_trios(count):
+    all_lines = []
     az = [a_min,a,a_max]
     bz = [b_min,b,b_max]
     c = 0.62
     d = 6
     e = 10
     for i in range(count):
-        make_trio(az,bz,c,d,e,pb)
-        ami = a * np.random.uniform(low=0.6, high=0.9)
-        amd = a * np.random.uniform(low=0.9, high=1.1)
-        ama = a * np.random.uniform(low=1.1, high=1.4)
-        az = [ami,amd,ama]
-        bmi = b * np.random.uniform(low=0.8, high=0.95)
-        bmd = b * np.random.uniform(low=0.95, high=1.05)
-        bma = b * np.random.uniform(low=1.75, high=1.225)
-        bz = [bmi,bmd,bma]
+        all_lines.extend(make_trio(az,bz,c,d,e))
+        az =  get_az()
+        bz = get_bz()
         c = np.random.uniform(low=0.23, high=0.85)
         d = int(np.random.uniform(low=2, high=28))
         e = int(np.random.uniform(low=7, high=14))
-
-
+    return all_lines
 
 #DEFINE STARTING VALUES
-a = 350.0
+starting_score = 350.0
+
+a = 1.0
 b = 0.05
 
-a_max = a * 1.2
+a_max = 1.2
 b_max = b * 1.15
 
-a_min = a * 0.8
+a_min = 0.8
 b_min = b * 0.85
 
 e = 10
 line_count = 20
 
-plot_start = 60
-plot_end = 720
+range_start = 60
+range_end = 720
 step = 8
-plot_range = np.arange(plot_start,plot_end,step)
+data_range = np.arange(range_start,range_end,step)
 
 rand_seed = 21
 np.random.seed(rand_seed)
 
-#PUT IT ALL TOGETHE
-output_file("base_line_bCoef.html")
-pb = figure(width = 1200, height = 600)
-#mini,mid,max  = make_line_trio_data(a_min,a,a_max,b_min,b,b_max)
-#add_bump_trio(mini,mid,max,bump_start,bump_end)
-#add_trio_to_plot(pb,mini,mid,max)
+csv_path = 'W:\Datasets\synth_scoring\lines.csv'
 
-make_trios(line_count,pb)
+#PUT IT ALL TOGETHER
+all_lines = make_trios(line_count)
 
-pb.title.text = "Min Max scale on B"
-pb.legend.location = "top_left"
-pb.grid.grid_line_alpha = 0
-pb.xaxis.axis_label = 'Seconds'
-pb.yaxis.axis_label = 'Score'
-pb.ygrid.band_fill_color = "olive"
-pb.ygrid.band_fill_alpha = 0.1
-show(pb)
+for line in all_lines:
+    line = np.asarray(line)
+
+all_lines = np.asarray(all_lines)
+np.savetxt(csv_path,all_lines)
--- Python/Machine Learning/SummerFeast/SummerChallengeScorer/SummerChallengeScorer/plot_data.py
+++ Python/Machine Learning/SummerFeast/SummerChallengeScorer/SummerChallengeScorer/plot_data.py
@@ -1,1 +1,50 @@
+import numpy as np
+from bokeh.plotting import figure, output_file, show
 
+#DEFINE INNER FUNCTIONS
+def plot_sin(steps):
+    sin = sin_vals(steps)
+
+    output_file("sine_line.html")
+    sp = figure(width=800, height=800)
+    sin_step = (np.pi * 2.0) / steps
+    x_range = np.arange(0,np.pi * 2.0 + 0.1,sin_step)
+    sp.line(x_range,sin,legend='sin_bump', alpha=0.7, line_width=4, color='green')
+    show(sp)
+
+    
+def add_trio_to_plot(plot, trio):
+    plot.line(data_range,trio[0], alpha=0.6 + np.random.normal(0,0.2), line_width=int(np.random.uniform(low=2.8, high=5.3)), color='green')
+    plot.line(data_range,trio[1], alpha=0.6 + np.random.normal(0,0.2), line_width=int(np.random.uniform(low=2.8, high=5.3)), color = 'navy')
+    plot.line(data_range,trio[2], alpha=0.6 + np.random.normal(0,0.2), line_width=int(np.random.uniform(low=2.8, high=5.3)), color='red')
+
+#DEFINE STARTING VALUES
+range_start = 60
+range_end = 720
+step = 8
+data_range = np.arange(range_start,range_end,step)
+rand_seed = 21
+np.random.seed(rand_seed)
+
+#PUT IT ALL TOGETHE
+output_file("base_line_bCoef.html")
+plot = figure(width = 1200, height = 600)
+
+csv_path = 'W:\Datasets\synth_scoring\lines.csv'
+lines = np.loadtxt(csv_path)
+trios = []
+for i in range(2,len(lines),3):
+    trio = [lines[i - 2][1:],lines[i - 1][1:],lines[i][1:]]
+    trios.append(trio)
+
+for trio in trios:
+    add_trio_to_plot(plot,trio)
+    
+plot.title.text = "All lines extracted from csv file"
+plot.legend.location = "top_right"
+plot.grid.grid_line_alpha = 0
+plot.xaxis.axis_label = 'Seconds'
+plot.yaxis.axis_label = 'Score'
+plot.ygrid.band_fill_color = "olive"
+plot.ygrid.band_fill_alpha = 0.1
+show(plot)
