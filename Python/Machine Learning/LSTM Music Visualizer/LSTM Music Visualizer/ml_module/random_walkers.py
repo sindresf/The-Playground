d7e881e99bf71c3eb4cd63ffdb988e93c87b4f88
sindresf <sindresfjermestad@gmail.com>
2017-08-11 13:33:58 +0200

    made random influencer random value pools. "Hughe" improvement in fps.

--- Python/Machine Learning/LSTM Music Visualizer/LSTM Music Visualizer/ml_module/random_walkers.py
+++ Python/Machine Learning/LSTM Music Visualizer/LSTM Music Visualizer/ml_module/random_walkers.py
@@ -5,10 +5,14 @@ class Random_move(Influencer):
     def __init__(self,conf):
         self.move_max = 0.1 #conf.move_max
         self.move_min = self.move_max / 2.0
+        self.move_pool_size = 19467 #cool
+        self.move_rand = np.random.rand(self.move_pool_size,2)
+        self.move_rand_index = 0
 
     def _move(self, arr):
-        rand = np.random.rand(2)
-        return np.asarray([x + (self.move_min - rand[r] * self.move_max) for r,x in enumerate(arr)])
+        rand = self.move_rand[self.move_rand_index]
+        self.move_rand_index = (self.move_rand_index + 1) % self.move_pool_size
+        return [x + (self.move_min - rand[r] * self.move_max) for r,x in enumerate(arr)]
 
     def influence(self, visual_objects):
         return np.apply_along_axis(self._move,1,visual_objects[0]), visual_objects[1]
@@ -21,16 +25,29 @@ class Random_move_and_shift(Random_move):
         super().__init__(conf)
         self.color_min = 0 #conf.color_min
         self.color_max = 255 #conf.color_max
-        self.color_rand_min = -5 #conf.color_rand_min
-        self.color_rand_max = 6 #conf.color_rand_max
+        self.color_rand_max = 7 #conf.color_rand_max
+        self.color_rand_min = 1 - self.color_rand_max
+
+        self.shift_pool_size = 19467 #cool
+        self.shift_c = np.random.rand(self.shift_pool_size)
+        self.shift_rands = np.random.randint(self.color_rand_min,self.color_rand_max,(self.shift_pool_size,3))
+        self.shift_index = 0
 
     def _color_shift(self,c):
-        rands = np.random.randint(self.color_rand_min,self.color_rand_max,3)
+        #rands = np.random.randint(self.color_rand_min,self.color_rand_max,3)
+        this_shift = self.shift_rands[self.shift_index]
+        shift_c = self.shift_c[self.shift_index]
+        self.shift_index = (self.shift_index + 1) % self.shift_pool_size
         color_yeah = lambda c,r: max(self.color_min, min(self.color_max, c + r))
-        r = color_yeah(c[0],rands[0])
-        g = color_yeah(c[1],rands[1])
-        b = color_yeah(c[2],rands[2])
-        return np.asarray((r,g,b))
+        if shift_c < 0.331:
+            r = color_yeah(c[0],this_shift[0])
+            return (r,c[1],c[2])
+        elif shift_c < 0.671:
+            g = color_yeah(c[1],this_shift[1])
+            return (c[0],g,c[2])
+        elif shift_c < 1.01:
+            b = color_yeah(c[2],this_shift[2])
+            return (c[0],c[1],b)
 
     def influence(self, visual_objects):
         points, colors = visual_objects
